8f95d803e1840f39b090e925e7c99fa05688a5c1
Vera <vera.serebryakova.24@mail.ru>
2022-09-10 23:27:48 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] getPointCoors(string PointName)
+{
+    int[] result = new int[3];
+    for (int i = 0; i  < 3; i++)
+    {
+        string[] CoorNames = {"X", "Y", "Z"};
+        Console.WriteLine($"Введите координату {CoorNames[i]} точки {PointName}:");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return result;
+}
+
+double Distance(double xA, double xB, double yA, double yB, double zA, double zB)
+{
+    return Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+}
+
+int[] PointA = getPointCoors("A");
+int[] PointB = getPointCoors("B");  
+Console.WriteLine(Math.Round(Distance(PointA[0], PointB[0], PointA[1], PointB[1], PointA[2], PointB[2]), 2));
